d6508f4fb4f8f9bcd578a7a9a471cd418a13357f
Alex Jung <jungdevelop@googlemail.com>
2024-12-22 23:55:30 +0100

    added commands to __init__

--- apyefa/commands/__init__.py
+++ apyefa/commands/__init__.py
@@ -1,8 +1,10 @@
 from .command import Command
 from .command_add_info import CommandAdditionalInfo
 from .command_departures import CommandDepartures
+from .command_line_list import CommandLineList
 from .command_serving_lines import CommandServingLines
 from .command_stop_finder import CommandStopFinder
+from .command_stop_list import CommandStopList
 from .command_system_info import CommandSystemInfo
 from .command_trip import CommandTrip
 
@@ -11,7 +13,9 @@ __all__ = [
     "CommandDepartures",
     "CommandAdditionalInfo",
     "CommandStopFinder",
+    "CommandStopList",
     "CommandSystemInfo",
+    "CommandLineList",
     "CommandTrip",
     "CommandServingLines",
 ]
